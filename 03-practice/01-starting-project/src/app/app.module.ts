c1e1164ebd834ea3b62be5a648ddab841062ec96
Bryan Arubang <arubangnbryan@gmail.com>
2024-09-02 21:44:21 +0800

    My First Commit

---
+++ 03-practice/01-starting-project/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { UserInputComponent } from "./user-input/user-input.component";
+import { InvestmentResultComponent } from "./investment-results/investment-results.component";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { UserInputModule } from "./user-input/user-input.module";
+
+
+@NgModule({
+    declarations: [
+        AppComponent, 
+        HeaderComponent, 
+        InvestmentResultComponent
+    ],
+    imports: [
+        BrowserModule,
+        UserInputModule
+    ],
+    bootstrap: [
+        AppComponent
+    ]
+})
+export class AppModule {
+
+}
